547dc2f6a4e3959483c382659b0b6d392a75e0e0
ramyus889 <ramyus889.com>
2024-12-03 15:46:28 +0300

    add

--- src/component/UseHome.vue
+++ src/component/UseHome.vue
@@ -3,7 +3,7 @@ import UsePinia from '../TaskComponent/UsePinia.vue';
 </script>
 
 <template>
-  <div class="sm:mt-20">
+  <div class="mt-3 sm:mt-20">
     <div class="">
       <UsePinia />
     </div>
